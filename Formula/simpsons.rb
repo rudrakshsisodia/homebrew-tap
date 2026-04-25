e7005efeefadb8b2179f9b0420f2788f25082b1e
Rudraksh sisodia <rudrakshsisodia@Rudrakshs-MacBook-Pro.local>
2026-04-25 07:57:36 +0530

    simpsons 0.1.1

--- Formula/simpsons.rb
+++ Formula/simpsons.rb
@@ -1,26 +1,26 @@
 class Simpsons < Formula
   desc "TUI analytics dashboard for Claude Code sessions"
   homepage "https://github.com/rudrakshsisodia/simpsons"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rudrakshsisodia/simpsons/releases/download/v0.1.0/simpsons_0.1.0_darwin_arm64.tar.gz"
-      sha256 "731b4095a288ec999b39c68699a6fb5dd66d2e9ea39a34fb5c228c7f838f7e38"
+      url "https://github.com/rudrakshsisodia/simpsons/releases/download/v0.1.1/simpsons_0.1.1_darwin_arm64.tar.gz"
+      sha256 "3db19769a1ec6b537ae9bf0a68adf7ccc864da0becbaa775b3db721385419a0f"
     else
-      url "https://github.com/rudrakshsisodia/simpsons/releases/download/v0.1.0/simpsons_0.1.0_darwin_amd64.tar.gz"
-      sha256 "230c5db65a72730f145ea9663855aa053f42cc588ae4d8bf5216ac02d7d717df"
+      url "https://github.com/rudrakshsisodia/simpsons/releases/download/v0.1.1/simpsons_0.1.1_darwin_amd64.tar.gz"
+      sha256 "417c52b3f8865c692d98187f3ca574c9c3497f1f2e6d8fcac19d348da3425c06"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/rudrakshsisodia/simpsons/releases/download/v0.1.0/simpsons_0.1.0_linux_arm64.tar.gz"
-      sha256 "96f9e3d6f25b5df3a56f44eadbc8413b3cfbfa5e40edb110386842aaa8328ced"
+      url "https://github.com/rudrakshsisodia/simpsons/releases/download/v0.1.1/simpsons_0.1.1_linux_arm64.tar.gz"
+      sha256 "20e4fbaf3f1c5f5444dc128bdc2026538b500483023331f359165ba4ab991891"
     else
-      url "https://github.com/rudrakshsisodia/simpsons/releases/download/v0.1.0/simpsons_0.1.0_linux_amd64.tar.gz"
-      sha256 "38e2ef110125166ff70f2d3b2f08357cc535b8ec13cdea71439bc8c80f96e3ba"
+      url "https://github.com/rudrakshsisodia/simpsons/releases/download/v0.1.1/simpsons_0.1.1_linux_amd64.tar.gz"
+      sha256 "9d94fb28831195805972dc2f17f824a6f128f0a78272d4d572a1389000ffd8f9"
     end
   end
 
